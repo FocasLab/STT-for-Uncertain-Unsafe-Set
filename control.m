ebbea260cbe6aacc48e277d48b3c3728d4da619d
Siddhartha Upadhyay <130141078+UpadhyaySid@users.noreply.github.com>
2026-05-12 13:19:20 +0530

    Update control.m

--- control.m
+++ control.m
@@ -1,4 +1,5 @@
 function u = control(tube,rad,robot)
+     %% control input as in Theorem 4.1 or Algorithm 1
     e = norm(robot - tube)/rad;
     k = 10;
     u = -k * (robot - tube) * log((1+e)/(1-e));
